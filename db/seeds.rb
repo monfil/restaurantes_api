b51b761f2a02486781152b594b71a60d2e541f60
m0nfil <fabiolamonfil@gmail.com>
2017-08-31 19:13:23 -0500

    Filled restaurants's table with data from a CSV file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('restaurantes.csv'))
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  t = Restaurant.new
+  t.id = row['id']
+  t.rating = row['rating']
+  t.name = row['name']
+  t.site = row['site']
+  t.email = row['email']
+  t.phone = row['phone']
+  t.street = row['street']
+  t.city = row['city']
+  t.state = row['state']
+  t.lat = row['lat']
+  t.lng = row['lng']
+  t.save
+end
