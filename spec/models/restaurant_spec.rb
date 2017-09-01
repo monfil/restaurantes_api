f02f8f9d14ec28c01e3b65d9840c863b86448cf9
m0nfil <fabiolamonfil@gmail.com>
2017-08-31 19:55:00 -0500

    Model specs

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it "Is valid with id" do
+		restaurant = Restaurant.new(
+			id: '9f98fad3-ecd6-421b-ba8c-2bd63340473d',
+			rating: 2,
+			name: 'Macías S.L.',
+			site: 'http://miguel.com.mx',
+			email: 'Jazmin_Santiago7@yahoo.com',
+			phone: '508272671',
+			street: '1736 Adela Urbanización',
+			city: 'Puerto Vallarta Dorotea',
+			state: 'Baja California Sur',
+			lat: 19.4369257779145,
+			lng: -99.1315607513514,
+			)
+	end
 end
